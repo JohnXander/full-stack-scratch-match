4ec292f1a56f073631070bd9a48a26f1ec119cad
John Xander <johnblox5@gmail.com>
2022-09-08 17:38:50 +0100

    register form not connected

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,34 @@
+import { useEffect, useState } from 'react';
+import UserForm from './components/UserForm';
+
 const App = () => {
+  const [registerResponse, setRegisterResponse] = useState("")
+
+  const handleRegister = async ({ username, email, password, countriesVisited, friends }) => {
+    try {
+      await fetch("http://localhost:4000/users/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ username, email, password, countriesVisited, friends })
+      })
+
+      setRegisterResponse("User successfully registered")
+      setTimeout(() => { setRegisterResponse("") }, "5000")
+
+    } catch (error) {
+      setRegisterResponse("Something went wrong")
+    }
+
+  };
+
   return (
-    <div>App</div>
+    <div>
+      <h1>Register</h1>
+      <UserForm handleSubmit={handleRegister} />
+      {registerResponse && <p>{registerResponse}</p>}
+    </div>
   )
 }
 
